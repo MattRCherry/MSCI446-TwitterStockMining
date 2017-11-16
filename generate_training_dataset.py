d10eef3f26d35269bb9136e668232c2daedf147d
Matt Cherry <mrcherry@uwaterloo.ca>
2017-11-16 11:59:46 -0500

    Reverted to original search option

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -4,13 +4,13 @@ from search_twitter_query import search_twitter_return_pop_ev
 from datetime import datetime, timedelta
 
 # Where to save the csv output
-file_name = 'training_data.csv'
+file_name = 'results.csv'
 
 field_labels = ['company', 'date', 'numTweets', 'avgSubj', 'avgPol', 'medianTime', 'sumRetweet',
                 'sumFavorite', 'sumFollowers', 'numVerified']
 
 # Include the twitter handles you would like to collect data for here
-companies = ['@google', '@intel', '@xerox', '@cisco']
+companies = ['@apple', '@microsoft', '@google', '@intel', '@hp', '@cisco', '@ibm', '@amazon', '@oracle', '@xerox']
 
 # Add dates from 'm' to 'n' days ago to a list
 # Change the values in 'range(m, n)' to change the search window
@@ -26,15 +26,15 @@ tr_data_list = []
 for company in companies:
     for date in dates:
 
-        # Search Option 1: All trading hour tweets (slower)
-        #result = search_twitter_return_ev(company,
-        #                                  datetime.strftime(date, '%Y-%m-%d'),
-        #                                  datetime.strftime(date + timedelta(days=1), '%Y-%m-%d'))
-
-        # Search Option 2: 'n' most popular tweets (faster, set n in search_twitter_query.py)
-        result = search_twitter_return_pop_ev(company,
+        # Search Option 1: All trading hour tweets (slower, but looks at all data)
+        result = search_twitter_return_ev(company,
                                           datetime.strftime(date, '%Y-%m-%d'),
                                           datetime.strftime(date + timedelta(days=1), '%Y-%m-%d'))
+
+        # Search Option 2: verified data only (faster, but looks at much less data)
+        # result = search_twitter_return_pop_ev(company,
+        #                                  datetime.strftime(date, '%Y-%m-%d'),
+        #                                  datetime.strftime(date + timedelta(days=1), '%Y-%m-%d'))
 
         # Append the results to our training data list
         tr_data_list.append(result)
