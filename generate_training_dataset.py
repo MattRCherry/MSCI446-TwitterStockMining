e794892234a0827cee77321ebb6a7c24bad496ff
yusuf <yusufkhaled@live.ca>
2017-11-22 14:49:20 -0500

    d`
    d

--- generate_training_dataset.py
+++ generate_training_dataset.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from search_twitter_query import search_twitter_return_ev
+from search_twitter_query import search_twitter_return_pop_ev
 from datetime import datetime, timedelta
 
 # Where to save the csv output
-file_name = 'training_data.csv'
+file_name = 'results.csv'
 
 field_labels = ['company', 'date', 'numTweets', 'avgSubj', 'avgPol', 'medianTime', 'sumRetweet',
                 'sumFavorite', 'sumFollowers', 'numVerified']
 
 # Include the twitter handles you would like to collect data for here
-companies = ['@microsoft']
+companies = ['@cisco']
 
 # Add dates from 'm' to 'n' days ago to a list
 # Change the values in 'range(m, n)' to change the search window
@@ -24,9 +25,17 @@ tr_data_list = []
 # Run search_twitter_return_ev function for each company-date combination in our lists.
 for company in companies:
     for date in dates:
+
+        # Search Option 1: All trading hour tweets (slower, but looks at all data)
         result = search_twitter_return_ev(company,
                                           datetime.strftime(date, '%Y-%m-%d'),
                                           datetime.strftime(date + timedelta(days=1), '%Y-%m-%d'))
+
+        # Search Option 2: verified data only (faster, but looks at much less data)
+        # result = search_twitter_return_pop_ev(company,
+        #                                  datetime.strftime(date, '%Y-%m-%d'),
+        #                                  datetime.strftime(date + timedelta(days=1), '%Y-%m-%d'))
+
         # Append the results to our training data list
         tr_data_list.append(result)
 
